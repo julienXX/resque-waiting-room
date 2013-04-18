f0d8ee97f018bdd2a782f7ab5e886b2a257788c2
Julien Blanchard <julien@sideburns.eu>
2013-04-18 11:24:54 +0200

    Use mock_redis

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,15 @@
 require 'resque'
+require 'mock_redis'
 require File.expand_path(File.join(File.dirname(__FILE__), '..', 'lib', 'resque-waiting-room'))
 
 RSpec.configure do |config|
   config.mock_framework = :rspec
 end
+
+puts "Using a mock Redis"
+r = MockRedis.new host: "localhost", port: 9736, db: 0
+$mock_redis = Redis::Namespace.new :resque, redis: r
+Resque.redis = $mock_redis
 
 # Require ruby files in support dir.
 Dir[File.expand_path('spec/support/*.rb')].each { |file| require file }
